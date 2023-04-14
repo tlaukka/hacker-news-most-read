e25831519e4d6f2251eaa45a948ff7e9693ad50d
Teemu Laukka <teemulaukka@gmail.com>
2023-04-14 16:26:06 +0300

    Add emotion/styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css'
+import styled from '@emotion/styled'
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <h1>Hacker News Most Read</h1>
-    </div>
+    </AppContainer>
   )
 }
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
 export default App
