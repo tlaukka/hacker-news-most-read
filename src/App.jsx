8eb8fe335f88ebfad15ad65fca6e924e2bbc45b0
Teemu Laukka <teemulaukka@gmail.com>
2023-04-14 17:30:18 +0300

    Add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import styled from '@emotion/styled'
 import { Outlet } from 'react-router-dom'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+async function getTopStories () {
+  const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+
+  if (!response.ok) {
+    throw new Error('Error loading top stories!')
+  }
+
+  return response.json()
+}
 
 function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Root />
+    </QueryClientProvider>
+  )
+}
+
+function Root () {
+  const { data } = useQuery({ queryKey: ['topstories'], queryFn: getTopStories })
+  console.log(data)
+
   return (
     <AppContainer>
       <h1>Hacker News Most Read</h1>
