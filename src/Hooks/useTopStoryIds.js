5511ebfb3397ce037e92caa34976b1971bc68088
Teemu Laukka <teemulaukka@gmail.com>
2023-04-15 15:57:12 +0300

    Add useTopStories hook

---
+++ src/Hooks/useTopStoryIds.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {  useQuery } from '@tanstack/react-query'
+import { fetchTopStories } from '../queries'
+
+function useTopStoryIds (pageSize = 20) {
+  const [page, setPage] = React.useState(0)
+
+  const { data: storyIds } = useQuery({
+    queryKey: ['topstories'],
+    queryFn: fetchTopStories,
+    enabled: page > 0
+  })
+
+  const getNextPage = React.useCallback(
+    () => {
+      setPage((prevPage) => prevPage + 1)
+    },
+    []
+  )
+
+  const end = page * pageSize
+  const topStoryIds = storyIds?.slice(0, end) ?? []
+
+  return {
+    topStoryIds,
+    getNextPage
+  }
+}
+
+export default useTopStoryIds
